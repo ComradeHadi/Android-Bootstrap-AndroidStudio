bb26fa2128d93bc816d6966467f449f4b71d5f48
dmitre work <eleet_89@mail.ru>
2014-02-14 12:20:27 +0300

    Initial commit

---
+++ AndroidBootstrapTest/src/com/beardedhen/androidbootstraptest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.beardedhen.androidbootstraptest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapEditText;
+import com.beardedhen.androidbootstrap.FontAwesomeText;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dmtest);
+	}
+
+}

d7c4dfe54005ec9a690ba89856425055f510264f
dmitre work <eleet_89@mail.ru>
2014-03-11 10:02:32 +0300

    Merge remote-tracking branch 'origin/master'

--- valentine/src/main/java/com/dmitry/valentine/MainActivity.java
+++ valentine/src/main/java/com/dmitry/valentine/MainActivity.java
@@ -1,12 +1,7 @@
 package com.dmitry.valentine;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
